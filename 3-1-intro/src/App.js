80764aca3df0092618831f00d4e441d6b05f1d77
cherrythefool <sarahandecherry@gmail.com>
2023-05-04 00:53:59 -0600

    redoingreact challenges - intro/react app

---
+++ 3-1-intro/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/Footer.js'
+import Content from './components/Content.js'
+import Header from './components/Header.js'
+function App() {
+  
+return <>
+    <Header/>
+    <Content/>
+    <Footer/>
+</>
+}
+
+export default App;
---
+++ 3-1-intro/src/components/Content.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Content = () => {
+    return <ul>
+        <li>Halie</li>
+        <li>Ramgel</li>
+        <li>CRod</li>
+        <li>Jessica</li>
+        <li>Rashaan</li>
+        <li>Kyle</li>
+        <li>Will</li>
+        <li>Justin</li>
+        <li>Yue Han</li>
+        <li>Aubrey</li>
+        <li>Bea</li>
+        <li>Tucker</li>
+        <li>Scott</li>
+        <li>Suri</li>
+        <li>Vance</li>
+        <li>Henri</li>
+        <li>Ernesto</li>
+        <li>Cherry</li>
+        </ul>
+}
+
+export default Content
