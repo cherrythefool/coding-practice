d76c752fb13e1012db613ffed7b0264be9e48ad2
cherrythefool <sarahandecherry@gmail.com>
2023-05-10 14:00:35 -0600

    old commit

--- 3-1-intro/src/App.js
+++ 3-1-intro/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Footer from './components/Footer.js'
 import Content from './components/Content.js'
 import Header from './components/Header.js'
+import Favorites from './components/Favorites.js'
+
 function App() {
   
 return <>
     <Header/>
+    <Favorites/>
     <Content/>
-    <Footer/>
+    <Footer/> 
 </>
 }
 
---
+++ 3-1-intro/src/components/Favorites.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Favorites = () => {
+    return <>
+        <h3>Favorites</h3>
+        <ul>
+            <li>bread!</li>
+            <li>Lizzi Lizardo</li>
+            <li>white water rafting</li>
+            <li>wildflowers</li>
+            <li>community</li>
+        </ul>
+        <br/>
+        </>
+}
+
+export default Favorites
